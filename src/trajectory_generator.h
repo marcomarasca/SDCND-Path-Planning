2b7046a402808413b1a4550c6d7e281e17c22800
Marco Marasca <marascamarco@gmail.com>
2018-11-21 22:41:00 -0800

    Trajectory generator documentation

--- src/trajectory_generator.h
+++ src/trajectory_generator.h
@@ -10,6 +10,10 @@ namespace PathPlanning {
 
 using Coeff = std::vector<double>;
 using CTrajectory = std::pair<std::vector<double>, std::vector<double>>;
+
+/**
+ * A function that returns the frenet state at the given point in time t
+ */
 using StatePredictionFunction = std::function<Frenet(double t)>;
 
 /**
@@ -28,9 +32,26 @@ class TrajectoryGenerator {
   TrajectoryGenerator(const Map &map, double step_dt, double max_speed, double max_acc);
   ~TrajectoryGenerator(){};
 
+  /**
+   * Generates a trajectory of the given length from the given starting state to the given target state, the trajectory
+   * does not include the start state
+   */
   FTrajectory Generate(const Frenet &start, const Frenet &target, size_t length) const;
+
+  /**
+   * Generates a trajectory of the given length from the given starting state, using the given state prediction
+   * function, the trakectory does not include the starting state
+   */
   FTrajectory Predict(const Frenet &start, const StatePredictionFunction &predict, size_t length) const;
+
+  /**
+   * Converts the given frenet trajectory into a trajectory of cartesian (global) coordinates
+   */
   CTrajectory FrenetToCartesian(const FTrajectory &trajectory) const;
+
+  /**
+   * Return the length of a trajectory of the given duration (in seconds) using the default step delta t
+   */
   size_t TrajectoryLength(double t) const;
 
  private:
